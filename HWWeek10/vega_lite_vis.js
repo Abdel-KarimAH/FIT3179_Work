4329841b9c636aa94ab3e974eb0487ea95d7e20c
Abdel-KarimAH <111498426+Abdel-KarimAH@users.noreply.github.com>
2022-10-09 15:24:08 +1100

    Update vega_lite_vis.js

--- HWWeek10/vega_lite_vis.js
+++ HWWeek10/vega_lite_vis.js
@@ -3,6 +3,6 @@ vegaEmbed("#map_visual", vg_1).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 var vg_2 = "FatalitiesByOperator.vg.json";
-vegaEmbed("#bubble_visual", vg_1).then(function(result) {
+vegaEmbed("#bubble_visual", vg_2).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
